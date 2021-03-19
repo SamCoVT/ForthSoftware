1a1282d748e12cb2cf693b3471ccf7dddea93119
SamCoVT <scolwell@nfatech.com>
2021-03-18 21:50:50 -0400

    Added spi support for TaliForth2 on SBC hardware

--- spi.fs
+++ spi.fs
@@ -7,8 +7,9 @@
 \ They will then be ported to TaliForth2 on 65C02.
 
 \ Hardware specific words:
+\ KL25Z with mecrisp
 hex
-: spi.setup
+: spi.init
     \ MISO = A4
     \ MOSI = A5
     \ CLK  = D4
@@ -38,6 +39,35 @@ hex
 
 : spi.miso? GPIOA_PDIR @  10 and ;
 
+
+
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\ TaliForth2 on SBC words for SD card over SPI
+$7F00 constant via.portb
+$7F02 constant via.ddrb
+: spi.init
+    \ Using VIA Port B for SPI
+    \ Bit 0 = CLK
+    \ Bit 1 = MOSI
+    \ Bit 2 = Slave Select
+    \ Bit 7 = MISO (using bit 7 for faster reads in assembly)
+    $04 via.portb c! \ Slave select high
+    $07 via.ddrb  c! \ Make CLK, MOSI, and SS outputs
+;
+
+: spi.ssHI   via.portb c@  $04 or   via.portb c! ;
+: spi.ssLO   via.portb c@  $FB and  via.portb c! ;
+: spi.mosiHI via.portb c@  $02 or   via.portb c! ;
+: spi.mosiLO via.portb c@  $FD and  via.portb c! ;
+: spi.clkHI  via.portb c@  $01 or   via.portb c! ;
+: spi.clkLO  via.portb c@  $FE and  via.portb c! ;
+: spi.clock  spi.clkHI spi.clkLO ;
+: spi.miso?  via.portb c@  $80 and ;
+
+
+
+
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 \ Generic SPI words:
 : >spi> ( u -- u ) \ send/receive byte over SPI bus
     \ MSb first
@@ -49,43 +79,98 @@ hex
     \ Chop result to just byte recieved.
     $ff and ;
 
- : >spi ( u -- )   >spi> drop ;
- : spi> ( -- u )   FF >spi> ;
+: >spi ( u -- )   >spi> drop ;
+: spi> ( -- u )   $FF >spi> ;
 
+: sd.r1 ( -- u )
+    \ Wait for a response.
+    \ Try up to 1000 times
+    1000 0 do  spi>
+        dup $80 and  0= if unloop exit then  drop
+    loop
+    ." SD CARD TIMEOUT "
+    $ff ( put a dummy value on stack )
+;
+
+: sd.r7 ( -- d true | u false )
+    ( flag is 0 for R1 response with error, true for full [32-bit] R7 response )
+    sd.r1 dup 1 > if false exit then
+    drop ( the R1 response )
+    spi> 8 lshift spi> or  ( upper half )
+    spi> 8 lshift spi> or  ( lower half )
+    swap ( make DOUBLE order)
+    true ;
+    
+
+: .sd.r1 ( u -- )
+    dup $40 and if ." PARAMETER ERROR " then
+    dup $20 and if ." ADDRESS ERROR " then
+    dup $10 and if ." ERASE SEQUENCE ERROR " then
+    dup $08 and if ." COM CRC ERROR " then
+    dup $04 and if ." ILLEGAL COMMAND " then
+    dup $02 and if ." ERASE RESET " then
+        $01 and if ." IDLE " then ;
+     
+ 
 : sd.cmd ( crc7 argLSB arg arg argMSB cmd# -- resp )
     %01000000 or >spi  ( Send command )
     >spi >spi >spi >spi ( Send argument)
     >spi ( Send CRC - always 0x95)
-    ( Wait for response )
-    begin $ff >spi>  dup $80 and while drop repeat ;
+    ;
 
 : sd.cmd0 ( -- resp )
-    $95 0 0 0 0 0 sd.cmd ;
+    spi.ssLO
+    $95 0 0 0 0 0 sd.cmd  sd.r1
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
+
 : sd.cmd1 ( -- resp )
-    0 0 0 0 0 1 sd.cmd ;
-: sd.cmd8 ( -- resp )
-   $87 $AA $01 00 00 08 sd.cmd ;
+    spi.ssLO
+    0 0 0 0 0 1 sd.cmd  sd.r1
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
+: sd.cmd8 ( -- resp f ) ( f=0 indicates failure and R1 response, F=true indicates R7 response)
+    spi.ssLO
+    $87 $AA $01 00 00 08 sd.cmd  sd.r7
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
+: sd.cmd58 ( -- resp f ) ( f=0 indicates failure and R1 response, F=true indicates R7 response)
+    spi.ssLO
+    0 0 0 0 0 #58 sd.cmd  sd.r7 \ Technicaly an R3 response, but format is identical.
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
 
 : sd.cmd55 ( -- )
-         0 0 0 0 0 #55 sd.cmd . space
-;
+    spi.ssLO
+    0 0 0 0 0 #55 sd.cmd  sd.r1
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
+
 : sd.acmd41 ( -- resp )
-      0 0 0 0 0 #55 sd.cmd . space
-      0 0 0 0 40 #41 sd.cmd . cr
-;
+    sd.cmd55  .
+    spi.ssLO
+    0 0 0 0 $40 #41 sd.cmd sd.r1 . cr
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
 
 
 : sd.init ( -- )
-    spi.setup
+    spi.init
     spi.sshi 80 0 do spi.clock loop
     spi.sslo
     sd.cmd0  1 = if
         cr ." Command 0 accepted" cr
-        ." Starting initialization (Command 1)."
-        begin sd.cmd1  1 and while ." ." repeat
-        ." COMPLETE" cr
-        ." Sending Command 8: "
-        5 0 do spi> . space loop cr
+\        ." Starting initialization (Command 1)."
+\        begin sd.cmd1  1 and while ." ." repeat
+\        ." COMPLETE" cr
+\        ." Sending Command 8: "
+\        5 0 do spi> . space loop cr
     then
 \    begin
 \        ." Send ACMD 41" cr
@@ -98,7 +183,8 @@ hex
 
 : readmbr
     \ Send command 17
-    0 0 0 0 0 #17 sd.cmd
+    spi.sslo
+    0 0 0 0 0 #17 sd.cmd sd.r1
     cr ." CMD17 sent - response =" . cr
     begin $ff >spi>  dup $ff = while  drop repeat
     ." Data token is " . cr
@@ -108,13 +194,14 @@ hex
         $ff >spi> . space
     loop
     cr
-    $ff >spi> ." CRC is " . cr ;
+    $ff >spi> ." CRC is " . cr
+
+    $FF >spi \ 1 extra idle byte with SS low.
+    spi.ssHI $FF >spi ; \ 1 extra idle byte with SS high.
+
 
 
 
 
     
-    
 
-
-    
