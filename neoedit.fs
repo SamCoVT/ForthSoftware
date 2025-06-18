0b35e887612e112d2860886d19ef3320f7302ae8
SamCoVT <scolwell@nfatech.com>
2025-06-18 16:52:37 -0400

    Adding initial code for neoedit on neo6502 platform.

---
+++ neoedit.fs
@@ -0,0 +1,80 @@
+\ PROGRAMMER  : Sam Colwell
+\ FILE        : neoedit.fs
+\ DATE        : 2025-06
+\ DESCRIPTION : Block editor for neo6502 using built-in editor
+
+\ Name the API locations
+$FF00              constant neoControlPort
+neoControlPort 1 + constant neoFunction
+neoControlPort 2 + constant neoError
+neoControlPort 3 + constant neoStatus
+neoControlPort 4 + constant neoParameters
+
+\ Variables needed by neoedit
+#80 buffer: neoErrorBuffer \ COUNTED STRING - Status info at bottom of screen
+#256 buffer: neoLineBuffer  \ COUNTED STRING - current line being edited
+\ Note: Counted strings have their length in the first byte.
+0 neoErrorBuffer c!
+#64 neoLineBuffer c!
+
+
+\ Bring Tali's assembler into the wordlist search order.
+assembler-wordlist >order
+
+\ Use the jump vectors to access these kernel routines.
+: neoReadCharacter [ $FFEE jsr ] ; always-native
+: neoWaitMessage   [ $FFF4 jsr ] ; always-native
+: neoSendMessage   [ $FFF7 jsr ] ; always-native
+\ Note: SendMessage expects two bytes after the JSR for the group
+\ and function number.  These can be added using
+\ [ group# c, function# c, ]
+\ just after using this word.
+
+: neoedit ( blk -- )
+  \ Save the block number in SCR so neoedit is interchangeable with LIST.
+  scr !
+  \ Start the editor using the API.
+  neoErrorBuffer neoParameters !
+  neoSendMessage [ 13 c, 1 c, ] neoWaitMessage
+  begin
+    \ Get return code
+    neoParameters c@   dup while
+    case
+      1 of ( Initialize )
+        \ Give the number of lines.
+        \ We're doing 16 lines of 64 chars, with linelength being
+        \ enforced by us (the editor doesn't know).
+        16 neoParameters ! endof
+      2 of ( Get Line )
+        \ Get the line number
+        neoParameters 1+ @
+        \ I believe this is one-based counting.
+        \ Make it zero-based.
+        1-
+        \ Get where line is in Tali buffer.
+        #64 *  scr @ block  +
+        \ Copy into counted string buffer.
+        neoLineBuffer 1+ #64 move
+        \ Tell the neo6502 editor where to find it.
+        neoLineBuffer neoParameters ! endof
+      3 of ( Get Key )
+        \ Just call the kernel getkey
+        neoSendMessage [ 2 c, 1 c, ] neoWaitMessage endof
+      4 of ( Put Line )
+        \ Get line number
+        neoParameters 1+ @
+        \ I believe this is one-based counting.
+        \ Make it zero-based.
+        1-
+        \ Get where line is in Tali buffer.
+        #64 *  scr @ block  +
+        \ Put the counted string there
+        neoLineBuffer count swap move   endof
+      ( Insert Line and Delete Line not supported and ignored )
+    endcase
+repeat ;
+        
+        
+  
+
+
